cca593c899ef79c6a029b99b72f106ee58c2f13f
Kaz <yinkdj@gmail.com>
2026-04-14 12:05:14 -0400

    cleaning up old commit

--- backend/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getDashboardSummary } = require("../controllers/dashboardController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.get("/summary", protect, getDashboardSummary);
-
-module.exports = router;
